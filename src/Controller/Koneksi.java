a750ca944a1391fd5a33abc6a2e2cc261969d281
Raihan <17radf@gmail.com>
2022-04-20 08:00:13 +0700

    minor refactor > remove manual loading driver class > refactor classes name > set sugar-coated loading thread sleep to 5 instead of 30 > enter event for login > maybe some minor changes

---
+++ src/Controller/Koneksi.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Anjayani
+ */
+
+public class Koneksi {
+
+	private static Connection koneksi;
+     
+public static Connection getKoneksi(){
+
+	if(koneksi==null){
+		try{
+			koneksi = DriverManager.getConnection("jdbc:mysql://localhost/db_presensi", "root", "");
+		} catch(SQLException ex) {
+		    JOptionPane.showMessageDialog(null, "Database Tidak Terhubung" + ex.getMessage());
+		}
+	}
+
+	return koneksi;
+
+} static Object getConnection(){
+
+        throw new UnsupportedOperationException("Tidak Dapat Terhubung");
+	}
+
+}
