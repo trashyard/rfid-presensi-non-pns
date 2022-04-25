5fc22768c748197cebb60c6f10970c4e764ad06a
Raihan <17radf@gmail.com>
2022-04-26 01:07:52 +0700

    add scrollbar for table and function for adding scroll style

--- src/com/presensikeun/form/Presensi.java
+++ src/com/presensikeun/form/Presensi.java
@@ -1,9 +1,46 @@
 package com.presensikeun.form;
 
-public class Presensi extends javax.swing.JPanel {
+import com.presensikeun.controller.Koneksi;
+import com.presensikeun.swing.ScrollBar;
+import java.awt.Color;
+import javax.swing.table.DefaultTableModel;
+import java.sql.*;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public final class Presensi extends javax.swing.JPanel {
+
+	Connection con = null;
+	ResultSet rs = null;
+	PreparedStatement pst = null;
 
 	public Presensi() {
+		this.con = Koneksi.getKoneksi();
 		initComponents();
+		tablePresensi();
+		table1.scroll(jScrollPane1);
+	}
+
+	public void tablePresensi() {
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("NIK");
+		model.addColumn("NAMA");
+		model.addColumn("STATUS");
+		model.addColumn("JABATAN");
+		model.addColumn("TANGGAL");
+		try {
+
+			String sql = "select tbk.id, tbk.nama, tdp.keterangan, tbj.nama, tdp.tanggal from tb_detail_presensi as tdp join tb_karyawan as tbk on tbk.id = tdp.id_karyawan join tb_jabatan as tbj on tbk.id_jabatan = tbj.id join tb_jadwal as jadwal on tdp.id_jadwal = jadwal.id order by tdp.tanggal desc";
+			pst = con.prepareStatement(sql);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				model.addRow(new Object[]{rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)});
+			}
+			table1.setModel(model);
+		} catch (SQLException ex) {
+			model.addRow(new Object[]{});
+			table1.setModel(model);
+		}
 	}
 
 	@SuppressWarnings("unchecked")
@@ -18,6 +55,7 @@ public class Presensi extends javax.swing.JPanel {
                 jLabel2 = new javax.swing.JLabel();
 
                 setBackground(new java.awt.Color(242, 246, 253));
+                setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
 
                 jLabel1.setFont(new java.awt.Font("sansserif", 1, 24)); // NOI18N
                 jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -26,9 +64,10 @@ public class Presensi extends javax.swing.JPanel {
 
                 panelShadow1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
 
-                jScrollPane1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(242, 246, 253), 2, true));
+                jScrollPane1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
 
                 table1.setBackground(new java.awt.Color(242, 246, 253));
+                table1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
                 table1.setModel(new javax.swing.table.DefaultTableModel(
                         new Object [][] {
                                 {null, null, null, null},
@@ -48,14 +87,14 @@ public class Presensi extends javax.swing.JPanel {
                         panelShadow1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(panelShadow1Layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 980, Short.MAX_VALUE)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 749, Short.MAX_VALUE)
                                 .addContainerGap())
                 );
                 panelShadow1Layout.setVerticalGroup(
                         panelShadow1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelShadow1Layout.createSequentialGroup()
-                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 373, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap()
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 352, Short.MAX_VALUE)
                                 .addContainerGap())
                 );
 
@@ -94,8 +133,8 @@ public class Presensi extends javax.swing.JPanel {
                                                 .addContainerGap()
                                                 .addComponent(jLabel1))
                                         .addComponent(panelShadow2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(0, 0, 0)
-                                .addComponent(panelShadow1, javax.swing.GroupLayout.PREFERRED_SIZE, 374, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(panelShadow1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 );
         }// </editor-fold>//GEN-END:initComponents
 
--- src/com/presensikeun/swing/Table.java
+++ src/com/presensikeun/swing/Table.java
@@ -4,6 +4,8 @@ import com.presensikeun.model.StatusType;
 import java.awt.Color;
 import java.awt.Component;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
@@ -29,27 +31,31 @@ public class Table extends JTable {
 		setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
 			@Override
 			public Component getTableCellRendererComponent(JTable jtable, Object o, boolean selected, boolean bln1, int i, int i1) {
-				if (i1 != 4) {
-					Component com = super.getTableCellRendererComponent(jtable, o, selected, bln1, i, i1);
-					com.setBackground(new Color(242, 246, 253));
-					setBorder(noFocusBorder);
-					if (selected) {
-						com.setForeground(new Color(15, 89, 140));
-					} else {
-						com.setForeground(new Color(102, 102, 102));
-					}
-					return com;
+				Component com = super.getTableCellRendererComponent(jtable, o, selected, bln1, i, i1);
+				com.setBackground(new Color(242, 246, 253));
+				setBorder(noFocusBorder);
+				if (selected) {
+					com.setForeground(new Color(15, 89, 140));
 				} else {
-					StatusType type = (StatusType) o;
-					CellStatus cell = new CellStatus(type);
-					return cell;
+					com.setForeground(new Color(102, 102, 102));
 				}
+				return com;
 			}
-		});
+
+		}
+		);
 	}
 
 	public void addRow(Object[] row) {
 		DefaultTableModel model = (DefaultTableModel) getModel();
 		model.addRow(row);
+	}
+
+	public void scroll(JScrollPane jspane) {
+		jspane.setVerticalScrollBar(new ScrollBar());
+		jspane.getVerticalScrollBar().setBackground(new Color(242, 246, 253));
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(85, 65, 118));
+		jspane.setCorner(JScrollPane.UPPER_RIGHT_CORNER, panel);
 	}
 }
